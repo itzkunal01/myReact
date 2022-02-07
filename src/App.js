d546b25aa9898c033e2c871cf413524688a62bf9
itzkunal01 <kunal.sharmag12@gmail.com>
2022-02-07 09:46:11 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +9,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
         <a
           className="App-link"
           href="https://reactjs.org"
