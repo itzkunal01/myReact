41efa116a253202fcccbc49c741c7dc51c409658
itzkunal01 <kunal.sharmag12@gmail.com>
2022-02-07 10:02:27 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -17,14 +17,6 @@ function App() {
         >
           Learn React
         </a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
